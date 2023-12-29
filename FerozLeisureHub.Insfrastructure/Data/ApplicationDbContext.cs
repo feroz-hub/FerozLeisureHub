70b00cdbf36d1bcbc9a1404078376abd6b892e35
feroz-hub <bashaferoz66@gmail.com>
2023-12-29 16:53:46 +0530

    Added

--- FerozLeisureHub.Insfrastructure/Data/ApplicationDbContext.cs
+++ FerozLeisureHub.Insfrastructure/Data/ApplicationDbContext.cs
@@ -1,9 +1,10 @@
 ﻿using FerozLeisureHub.Domain.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace FerozLeisureHub.Insfrastructure.Data
 {
-    public class ApplicationDbContext : DbContext
+    public class ApplicationDbContext : IdentityDbContext
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
         {
